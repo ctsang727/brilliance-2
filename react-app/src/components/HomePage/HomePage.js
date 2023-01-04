4c65a7ba25f392ac08feb8a8e06e05eedfd2da7d
Chris Tsang <christopher.tsang@uconn.edu>
2023-01-04 17:19:30 -0500

    album cover fix

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -36,7 +36,7 @@ const HomePage = () => {
 
         <div id='track-cont'>
         {tracks?.map(({id, lyrics, title, artist, userId, album_image}) => {
-          if(!album_image) album_image = 'https://www.mcicon.com/wp-content/uploads/2021/01/Music_Music_note_1-copy-5.jpg'
+          if(!album_image) album_image = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9398c530-d0a7-4c46-99b4-423a6aabf39f/d3kxnbe-f16dabfb-0cf1-436c-9315-915fbe462f23.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkzOThjNTMwLWQwYTctNGM0Ni05OWI0LTQyM2E2YWFiZjM5ZlwvZDNreG5iZS1mMTZkYWJmYi0wY2YxLTQzNmMtOTMxNS05MTVmYmU0NjJmMjMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Gk0TAQTWUZBrHFjRC0y7pMEEdep95g7p6upVZNpXaZg'
           return(
             <NavLink key={id} to={{pathname: `/tracks/${id}`, state: {id, lyrics, title, artist, userId, album_image}}}>
               <div className='single-track-list'>
